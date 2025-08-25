8780f0326d22f0b0d881041f85f208e6fb64968f
Oasis777999 <collegeoasis915@gmail.com>
2025-08-25 18:01:10 +0530

    Updated the mail in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -38,7 +38,7 @@ const Footer = () => {
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-blue-400" />
-                <span className="text-gray-300">info@chcmachinetracker.com</span>
+                <span className="text-gray-300">harishje55@gmail.com</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Clock className="w-5 h-5 text-blue-400" />
